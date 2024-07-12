3ad3bc312ede984bf47cb88930e7a98db7a88026
serxa <sergei@clickhouse.com>
2024-07-12 18:45:01 +0000

    add comment

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make
+
+# Simpler way to build without cmake and with dynamic linking
+#mkdir -p build && clang++-11 main.cpp -o build/stress_memcg -O3 -pthread -fno-omit-frame-pointer
