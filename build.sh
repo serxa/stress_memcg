2718d5f1134a776a84580acf4485814fe061c7a0
serxa <sergei@clickhouse.com>
2024-07-15 11:18:21 +0000

    get rid of cross-compilation

--- build.sh
+++ build.sh
@@ -36,19 +36,8 @@ echo "CXX=$CXX"
 #    export CXXFLAGS="-stdlib=libc++"
 #fi
 
-if [ "$1" = "aarch64" ]; then
-    if [ ! -e "/usr/aarch64-linux-gnu" ]; then
-        echo "Tools for aarch64 cross-compilation not detected, try to install with:"
-        echo "    sudo apt-get update"
-        echo "    sudo apt-get install gcc-aarch64-linux-gnu"
-        exit 1
-    fi
-    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-aarch64.cmake ..
-    make
-else
-    cmake ..
-    make
-fi
+cmake ..
+make
 
 # Simpler way to build without cmake, but with dynamic linking
 #mkdir -p build && $CXX main.cpp -o build/stress_memcg -O3 -pthread -fno-omit-frame-pointer
